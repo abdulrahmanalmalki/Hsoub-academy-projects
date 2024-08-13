ea579d95bba8d8fa0fe59ad9950b1cb67234d27e
abdulrahmanalmalki <113034994+abdulrahmanalmalki@users.noreply.github.com>
2024-08-13 16:20:37 +0300

    Add files via upload

---
+++ JavaScript path/event-booking-app/middlewares/isLogin.js
@@ -0,0 +1,8 @@
+const { AuthenticationError } = require("apollo-server-express");
+const isLoggedIn = (parent, args, {user}, info) => {
+    if (!user) {
+        throw new AuthenticationError("يجب تسجيل دخولك");
+    }
+};
+
+module.exports = { isLoggedIn };
---
+++ JavaScript path/event-booking-app/resolvers/auth.js
@@ -0,0 +1,60 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server-express");
+
+const authResolver = {
+  Mutation: {
+    createUser: async (_, args) => {
+      try {
+        const existingUser = await User.findOne({
+          email: args.userInput.email,
+        });
+        if (existingUser) {
+          throw new UserInputError("هذا الحساب موجود مسبقا لدينا", {
+            invalidArgs: args.userInput.email,
+          });
+        }
+        const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+        const user = new User({
+          username: args.userInput.username,
+          email: args.userInput.email,
+          password: hashedPassword,
+        });
+        await user.save();
+        const userForToken = {
+          email: user.email,
+          id: user.id,
+        };
+        return {
+          userId: user.id,
+          token: jwt.sign(userForToken, process.env.JWT_SECRET),
+          username: user.username,
+        };
+      } catch (err) {
+        throw err;
+      }
+    },
+    login: async (_, { email, password }, context) => {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        throw new UserInputError("هذا الحساب غير موجود لدينا");
+      }
+      const isEqual = await bcrypt.compare(password, user.password);
+      if (!isEqual) {
+        throw new UserInputError("خطا في البريد الالكتروني او كلمه المرور");
+      }
+      const userForToken = {
+        email: user.email,
+        id: user.id,
+      };
+      return {
+        userId: user.id,
+        token: jwt.sign(userForToken, process.env.JWT_SECRET),
+        username: user.username,
+      };
+    },
+  },
+};
+
+module.exports = { authResolver };
